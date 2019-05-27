0990756572569dc68a231af6361a709cb1235090
faker <2975777969@qq.com>
2019-05-27 17:15:22 +0800

    version 1.0

--- main/main.js
+++ main/main.js
@@ -1,4 +1,26 @@
-module.exports = function main(inputs) {
+module.exports = function main(input) {
     // write your code here...
-    return "price";
+    var distance=input.distance;	//注意输入是对象形式
+	var parkTime=input.parkTime;
+	if(distance<2){
+		return round(case1(parkTime));
+	}
+	else if(distance<=8){
+		return round(case2(distance,parkTime));
+	}
+	else if(distance>8){
+		return round(case3(distance,parkTime));
+	}
 };
+function case1(parkTime){
+	return 6+parkTime*0.25;
+}
+function case2(distance,parkTime){
+	return case1(parkTime)+(distance-2)*0.8;
+}
+function case3(distance,parkTime){
+	return case2(8,parkTime)+(distance-8)*0.8*1.5;
+}
+function round(cost){
+	return Math.round(cost);
+}
